07d7abf77698ea640e7e4209a7ac155d44f72413
DESKTOP-05PR4TA\Miyazawa.Fuki <tyukajin2014@gmail.com>
2017-09-11 12:18:12 +0900

    # 変更点 サウンド、フェード、オブジェクト生成のマネージャの追加、効率化 宮澤の作業用シーンの作成

---
+++ Misoten/Assets/Resources/Scripts/Managers/FadeManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;               // UIを使用可能にする
+using UnityEngine.SceneManagement;  // シーン遷移の関数を呼び出すために必要
+
+public class FadeManager : MonoBehaviour {
+    // 注意-----------------------------
+    // ビルドしてシーンの登録しといてね
+    // ---------------------------------
+    public enum FADE_MODE           // フェードの状態
+    {
+        FADE_NONE = 0,
+        FADE_IN,
+        FADE_OUT,
+    }
+    public Sprite texture;          // テクスチャ こっちに張り付けてね
+    [Range(0, 1.0f)]
+    public float speed = 1.0f;     // 透明化の速さ
+
+
+    GameObject obj;             // 自分の
+    public GameObject _child;   // パネルに使われるオブジェクト
+    public Image childColor;           //子オブジェクトのRGBA値
+    FADE_MODE mode;             // フェードの状態
+    Color color;                // 色の値
+    string nextScene;           // 次のシーン
+
+    // Startの前に行われる処理
+    private void Awake()
+    {
+        // 破棄を無効化
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        nextScene = null;           // 次のシーン初期化
+        mode = FADE_MODE.FADE_NONE;
+
+        // 色の初期化
+        color.r = childColor.color.r;
+        color.g = childColor.color.g;
+        color.b = childColor.color.b;
+        color.a = 0;
+
+        SetTexture(texture);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (mode != FADE_MODE.FADE_NONE)
+        {
+            // 画面を暗転化する
+            if (mode == FADE_MODE.FADE_OUT)
+            {
+                childColor.color = new Color(color.r, color.g, color.b, color.a);
+                color.a += speed / 100;
+            }
+            // 画面を見えるように戻す
+            else if (mode == FADE_MODE.FADE_IN)
+            {
+                childColor.color = new Color(color.r, color.g, color.b, color.a);
+                color.a -= speed / 100;
+            }
+
+            // 制御
+            if (color.a > 1.0f)
+            {
+                Debug.Log("画面暗転終了");
+                // フェードINへ移行
+                mode = FADE_MODE.FADE_IN;
+                if( nextScene != null )
+                {
+                    // シーン移行
+                    nextScene = null;
+                    SceneManager.LoadScene(nextScene);
+                }
+            }
+            else if (color.a < 0)
+            {
+                Debug.Log("フェード終了");
+                // フェード終了
+                color.a = 0;
+                mode = FADE_MODE.FADE_NONE;
+            }
+        }
+
+    }
+
+    // オブジェ取得
+    GameObject GetFade()
+    {
+        return obj;
+    }
+
+    // テクスチャの貼り付け
+    void SetTexture(Sprite tex)
+    {
+        childColor.sprite = tex;
+    }
+
+    // フェード状態取得
+    public FADE_MODE GetFadeMode()
+    {
+        return mode;
+    }
+
+    // 次のシーンへ行くフェード
+    public bool SetNextFade(string next)
+    {
+        if (mode == FADE_MODE.FADE_NONE)
+        {
+            Debug.Log("フェード呼び出し : " + next);
+
+            color.a = 0;                // アルファ値初期化
+            nextScene = next;           // 次のシーン
+            mode = FADE_MODE.FADE_OUT;  // フェードOUTへ
+            return true;
+        }
+        return false;
+    }
+
+    // 暗転だけのフェード
+    public bool SetFade()
+    {
+        if (mode == FADE_MODE.FADE_NONE)
+        {
+            Debug.Log("暗転フェード呼び出し" );
+            color.a = 0;                // アルファ値初期化
+            mode = FADE_MODE.FADE_OUT;  // フェードOUTへ
+            return true;
+        }
+        return false;
+    }
+}
