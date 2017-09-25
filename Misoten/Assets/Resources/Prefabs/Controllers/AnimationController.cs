ac83a2c678b58ac14e259db7eff4abd6a882464f
DESKTOP-05PR4TA\Miyazawa.Fuki <tyukajin2014@gmail.com>
2017-09-25 14:16:48 +0900

    ユニティちゃん # 変更点 ユニティちゃんアセットの追加 ※これらはいらなくなったら消す ボリュームに応じて変化するバーの実装

---
+++ Misoten/Assets/Resources/Prefabs/Controllers/AnimationController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    public Animator animator;               //自身のアニメーターを設定
+    public float speed;
+    public Rigidbody rigid;
+    public float movePower;
+
+    Vector3 oldRot;
+	// Use this for initialization
+	void Start () {
+        oldRot = Vector3.zero;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(animator == null)
+        {
+            return;
+        }
+
+        transform.Rotate(0,Input.GetAxis("Horizontal"),0);
+
+		if(Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("Walk", true);
+            rigid.AddForce(transform.forward * movePower);
+        }
+
+        else
+        {
+            animator.SetBool("Walk", false);
+        }     
+    }
+}
---
+++ Misoten/Assets/Resources/Scripts/Controller/SoundMeterController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMeterController : MonoBehaviour {
+
+    public SoundMeterImageController[] SMI;//音量バーのオブジェクト。小さい順に入れる
+
+    public float volume;//ボリューム。publicなのはデバッグ用
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        int countDraw = 0;
+
+		for(int i = 0; i < SMI.Length; i++)
+        {
+            if(SMI[i].SetIsDraw(volume))
+            {
+                countDraw++;
+            }
+        }
+
+        //ボリュームがMAXに対してどのくらいの割合
+        //であるかを基準としてメーターの色を決める
+        //赤、黄色、緑の三色を使用する
+        float per = (float)countDraw / (float)SMI.Length;//割合を計算
+
+        Color imageColor;
+
+        //一定以下の割合なら赤色を設定
+        if(per < 1.0f / 3.0f)
+        {
+            imageColor = Color.red;
+        }
+        else if(per < 2.0f / 3.0f)
+        {
+            imageColor = Color.yellow;
+        }
+        else
+        {
+            imageColor = Color.green;
+        }
+
+        for (int i = 0; i < countDraw; i++)
+        {
+            SMI[i].SetColor(new Vector3(imageColor.r,imageColor.g,imageColor.b));
+        }
+	}
+}
