05920a33dfbab53b6fad6d929576551b5b3773ce
DESKTOP-05PR4TA\Miyazawa.Fuki <tyukajin2014@gmail.com>
2017-10-26 12:03:47 +0900

    Curve # 変更点 プレイヤーが自動でカーブするように変更 ※作業中 モデルを自転車に差し替え

---
+++ Misoten/Assets/Resources/Scripts/Game/GoalManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour {
+
+    bool isFinished;
+
+	// Use this for initialization
+	void Start () {
+        isFinished = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //ゴールした時の処理
+    void Goal(PlayerController player)
+    {
+        ResultWorks.SetTime(player.GetPlayerNumber(),);
+        if (isFinished)
+        {
+            return;
+        }
+        //リザルト画面へ移行
+        FadeManager.SetNextFade("Result");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //プレイヤーと接触したらゴールしたとみなしゴール後の処理を行う
+        if(other.gameObject.tag == "Player")
+        {
+            //ゴール処理
+            Goal(other.GetComponent<PlayerController>());
+        }
+    }
+}
