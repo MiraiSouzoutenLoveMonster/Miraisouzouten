52bc38d5ce7cdab0af6a9906e8c5c5e97e71c11c
DESKTOP-05PR4TA\Miyazawa.Fuki <tyukajin2014@gmail.com>
2017-12-20 15:23:29 +0900

    BrushUp_20171220_2 # 変更点 ヒロインと父のボトルをとったときのカットインの追加

--- Misoten/Assets/Resources/Scripts/CutIn/FatherCutIn.cs
+++ Misoten/Assets/Resources/Scripts/CutIn/FatherCutIn.cs
@@ -1,16 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FatherCutIn : CutIn {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Image cut;//フェードインさせる対象
+    public Transform moveTarget;//フェードインの目標地点
+    public float moveSpeed;//フェードイン・アウトするスピードの係数
+    public float StayTime;//フェードインしてから待機する時間
+
+    float countMoveTime;
+    float countStayTime;
+    Vector3 initPos;
+
+    // Use this for initialization
+    void Start () {
+        countMoveTime = 0.0f;
+        countStayTime = 0.0f;
+        initPos = cut.transform.position;
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        CountTime();
+        MoveAnimation();
+    }
+
+    void MoveAnimation()
+    {
+        countMoveTime += Time.deltaTime * moveSpeed;
+
+        if (countMoveTime <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        if (countMoveTime >= 1.0f)
+        {
+            countMoveTime = 1.0f;
+            countStayTime += Time.deltaTime;
+            if (countStayTime >= StayTime)
+            {
+                moveSpeed *= -1.0f;
+            }
+        }
+
+        Vector3 newPos = Vector3.Lerp(initPos, moveTarget.position, countMoveTime);
+
+        cut.transform.position = newPos;
     }
 }
--- Misoten/Assets/Resources/Scripts/CutIn/HeroineCutIn.cs
+++ Misoten/Assets/Resources/Scripts/CutIn/HeroineCutIn.cs
@@ -5,13 +5,49 @@ using UnityEngine.UI;
 
 public class HeroineCutIn : CutIn {
 
+    public Image cut;//フェードインさせる対象
+    public Transform moveTarget;//フェードインの目標地点
+    public float moveSpeed;//フェードイン・アウトするスピードの係数
+    public float StayTime;//フェードインしてから待機する時間
+
+    float countMoveTime;
+    float countStayTime;
+    Vector3 initPos;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        countMoveTime = 0.0f;
+        countStayTime = 0.0f;
+        initPos = cut.transform.position;
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate() {
-        CountTime();
+        MoveAnimation();
 	}
+
+    //画像をアニメーションさせる。アニメーションが終わってから一定秒数たったらオブジェクトを破棄
+    void MoveAnimation()
+    {
+        countMoveTime += Time.deltaTime * moveSpeed;
+
+        if(countMoveTime <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        if(countMoveTime >= 1.0f)
+        {
+            countMoveTime = 1.0f;
+            countStayTime += Time.deltaTime;
+            if(countStayTime >= StayTime)
+            {
+                moveSpeed *= -1.0f;
+            }
+        }
+
+        Vector3 newPos = Vector3.Lerp(initPos, moveTarget.position, countMoveTime);
+
+        cut.transform.position = newPos;
+    }
 }
